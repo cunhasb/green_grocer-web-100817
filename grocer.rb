d1423622e5a41c6c01fc8b2a9a205341c701abb7
cunhasb CUNHA <cunhasb@aol.com>
2017-09-27 04:04:55 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -55,9 +55,9 @@ end
 def checkout(cart, coupons)
   # code here
   new_cart= apply_clearance(apply_coupons(consolidate_cart(cart),coupons))
+  binding.pry
   subtotal = 0.0
   total = 0.0
-  binding.pry
   new_cart.each {|items,attr| subtotal =+ attr[:price]}
   end
   (subtotal > 100) ? (total = subtotal * 0.90) : (total = subtotal)
